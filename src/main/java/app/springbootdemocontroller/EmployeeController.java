2772ffb9f4c005203f5c786d2e61c68a536eea20
Rammohan Surukonti <rammohan.surukonti@itsd-consulting.de>
2018-06-14 14:36:26 +0200

    Initial commit

--- src/main/java/app/springbootdemocontroller/EmployeeController.java
+++ src/main/java/app/springbootdemocontroller/EmployeeController.java
@@ -1,52 +1,42 @@
 package app.springbootdemocontroller;
 
+
 import app.springbootdemomodel.Employee;
 import app.springbootdemorepository.EmployeeRepository;
+import app.springbootdemoservice.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
-//it is the combination of spring controller and responsebody
-//controller says to define controller and responsebody is to indicate return value of method should be used as r b of request
 @RestController
 @RequestMapping(value="/api")
 public class EmployeeController {
 
-@Autowired
-EmployeeRepository repository;
+	@Autowired
+	EmployeeService employeeService;
 
+	@Autowired
+	EmployeeRepository repository;
 
-//is a short form of requestmapping and requesttype is get
-@GetMapping(value="/employee",  produces=MediaType.APPLICATION_JSON_VALUE)
-public List<Employee> getAll() {
-	List<Employee> list = new ArrayList();
-	Iterable<Employee> employees = repository.findAll();
+	@GetMapping(value="/employee",  produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> getAll() {
+		return employeeService.getAll();
+	}
 
-	employees.forEach(list::add);
-	return list;
-}
+	@PostMapping(value="/postemployee" ,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Employee postEmployee(@RequestBody Employee employee) {
+		return employeeService.postEmployee(employee);
+	}
 
-@PostMapping(value="/postemployee" ,consumes=MediaType.APPLICATION_JSON_VALUE)
-public Employee postEmployee(@RequestBody Employee employee) {
-//requestbody is to bind the request body with a method parameter
-	repository.save(employee);
-	return employee;
-}
+	@GetMapping(value="/findbylastname/{lastName}",  produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> findByLastName(@PathVariable("lastName") String lastName) {
+		return employeeService.findByLastName(lastName);
+	}
 
-@GetMapping(value="/findbylastname/{lastName}",  produces=MediaType.APPLICATION_JSON_VALUE)
-public List<Employee> findByLastName(@PathVariable("lastName") String lastName) {
-
-	List<Employee> employees = repository.findByLastName(lastName);
-	return employees;
-}
-
-
-@DeleteMapping(value="/employee/{id}")
-public void deleteEmployee(@PathVariable long id){
-	
-	repository.deleteById(id);
-}
+	@DeleteMapping(value="/employee/{id}")
+	public void deleteEmployee(@PathVariable long id){
+		employeeService.deleteEmployee(id);
+	}
 }
---
+++ src/main/java/app/springbootdemoservice/EmployeeService.java
@@ -0,0 +1,40 @@
+package app.springbootdemoservice;
+
+
+import app.springbootdemomodel.Employee;
+import app.springbootdemorepository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getAll() {
+        List<Employee> list = new ArrayList<>();
+        Iterable<Employee> employees = employeeRepository.findAll();
+        employees.forEach(list::add);
+        return list;
+    }
+
+    public Employee postEmployee(Employee employee) {
+        employeeRepository.save(new Employee(employee.getFirstName(), employee.getLastName()));
+        return employee;
+    }
+
+    public List<Employee> findByLastName(String lastName) {
+
+        List<Employee> employees = employeeRepository.findByLastName(lastName);
+        return employees;
+    }
+
+    public void deleteEmployee(long id){
+
+        employeeRepository.deleteById(id);
+    }
+}
